6600add7ff0d177b32e25ee1bfefa67fedb5bd4d
ErnestsGriezis <ernestsgg@gmail.com>
2024-11-28 13:18:12 +0200

    js-manipulation

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const body = document.body
+// const ul = document.createElement("ul")
+const ul = document.querySelector("ul")
+const l1 = document.querySelector("#li1")
+const li2 = document.querySelector("#li2") 
+li1.remove()
+//div.textContent = "Hi world"
+//body.append(div)
+
+//ul.innerHTML = "<li> 10317</li>"
+//body.append(ul)
+
+//const ul = document.querySelector("ul")
+
+//console.log(ul.textContent)
+//console.log(ul.innerText)
